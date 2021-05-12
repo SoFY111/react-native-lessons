a37405ed0ebd432608494f02c4f30747ca997464
Hakan DİNÇTÜRK <hakan.dincturkk@gmail.com>
2021-05-12 16:25:17 +0300

    firstCommit

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Button, SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+
+
+const Header = ({data, setData}) => {
+
+    const add = () => {
+        setData([...data, ''])
+    }
+
+    const reset = () => {
+        setData([''])
+    }
+
+    return (
+        <View style={styles.header}>
+            <Button title="Ekle" onPress={add}/>
+            <Button title="Reset" onPress={reset}/>
+        </View>
+    )
+}
+
+const Hey = ({id}) => {
+    return (
+        <View style={styles.hey}>
+            <Text>Alan {id + 1}</Text>
+        </View>
+    )
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [data, setData] = useState([''])
+
+    return (
+        <SafeAreaView>
+            <ScrollView onContentSizeChange={() => console.log('içerik değişti')}
+                        onMomentumScrollBegin={() => console.log('başladı')} stickyHeaderIndices={[0]}
+                        onMomentumScrollEnd={() => console.log('bitti')}
+                        maximumZoomScale={3} indicatorStyle={"default"} decelerationRate={"fast"}
+                        snapToInterval={2} snapToAlignment={'center'}
+                        scrollIndicatorInsets={{
+                            top: 100
+                        }}
+            >
+                <Header {...{data, setData}}/>
+                {data.map((d, i) => (
+                    <Hey id={i}/>
+                ))}
+            </ScrollView>
+        </SafeAreaView>
+    )
+        ;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        backgroundColor: 'purple',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    header: {
+        backgroundColor: '#fff',
+        flex:1,
+        flexDirection:'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20
+    },
+    hey: {
+        backgroundColor: 'lime',
+        padding: 20,
+        width: 500,
+        marginBottom: 10
+    }
 });
